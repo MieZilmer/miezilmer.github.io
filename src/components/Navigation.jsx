f92c2e627445e3bf2a1b2695bb69a6eb4b33beb2
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-09-03 11:00:10 +0200

    Portfolio setup

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router";
+
+export default function BannerImage() {
+  return (
+  <nav>
+    <NavLink to="/" end>
+      Home
+    </NavLink>
+    <NavLink to="/projects">Projects</NavLink>
+    <NavLink to="/about">About</NavLink>
+    <NavLink to="/contact">Contact</NavLink>
+  </nav>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Here are my projects...</p>
+    </div>
+  );
+}
+
+export default Projects;
