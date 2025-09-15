fc22635ea5122eebcb4345313395ceaa6e83ea58
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-09-15 14:44:27 +0200

    Checkpoint from VS Code for coding agent session

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Navigation from "./components/Navigation";
+import Links from "./pages/Links";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Routes>
+      {/* Routes go here */}
+      <Route path="/" element={<Home />} />
+      <Route path="/links" element={<Links />} />
+    </Routes>
   );
 }
+
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import Projects from "../components/Projects";
+
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to my portfolio website!</p>
+      <Projects />
     </div>
   );
 }
-
 export default Home;
